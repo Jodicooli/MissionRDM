ae14bf7799da4efbedc8b6682e1bbf424d530e0f
Jodicooli <johanna.summermatter@outlook.com>
2025-05-28 20:14:04 +0200

    rename folders

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Introduction from '@/pages/Introduction/Introduction.vue'
+import Tutorial from '@/pages/Introduction/Tutorial.vue'
 import StartScreen from '@/pages/StartScreen.vue'
+import Level1 from '@/pages/Level1/Level1.vue'
+import Level2 from '@/pages/Level2/Level2.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'StartScreen',
+    component: StartScreen
+  },
+  {
+    path: '/introduction',
+    name: 'Introduction',
+    component: Introduction
+  },
+  {
+    path: '/tutorial',
+    name: 'Tutorial',
+    component: Tutorial
+  },
+  {
+    path: '/level/1',
+    name: 'Level1',
+    component: Level1
+  },
+  {
+    path: '/level/2',
+    name: 'Level2',
+    component: Level2
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-  { path: '/', component: StartScreen },
-  { path: '/introduction', component: () => import('@/pages/introduction/Introduction.vue') },
-  { path: '/tutorial', component: () => import('@/pages/introduction/Tutorial.vue') },
-  { path: '/level/1', component: () => import('@/pages/level1/Level1.vue') },
-  { path: '/level/2', component: () => import('@/pages/level2/Level2.vue') },
- ]
+  routes
 })
 
 export default router
