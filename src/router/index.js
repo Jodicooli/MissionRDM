c1d2d26310d4a18ab60d9802828f31436409c16b
Jodicooli <johanna.summermatter@outlook.com>
2025-05-28 19:49:28 +0200

    fixed error

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import StartScreen from '@/pages/StartScreen.vue'
 
-const routes = [
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
   { path: '/', component: StartScreen },
   { path: '/introduction', component: () => import('@/pages/introduction/Introduction.vue') },
   { path: '/tutorial', component: () => import('@/pages/introduction/Tutorial.vue') },
   { path: '/level/1', component: () => import('@/pages/level1/Level1.vue') },
   { path: '/level/2', component: () => import('@/pages/level2/Level2.vue') },
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
+ ]
 })
 
 export default router
